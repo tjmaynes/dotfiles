8de2d933e65af3f3b3c944bfc63240a2f90364e3
TJ Maynes <tj@tjmaynes.com>
2023-09-17 18:40:40 -0500

    chore(.custom-fns.sh): add initial file

---
+++ .custom-fns.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function backup_projects()
+{
+    if [[ -z "$PROJECTS_DIRECTORY" ]]; then
+        echo "Please set 'PROJECTS_DIRECTORY' environment variable before running this command..."
+    elif [[ -z "$BACKUPS_DIRECTORY" ]]; then
+        echo "Please set 'BACKUPS_DIRECTORY' environment variable before running this command..."
+    else
+        NOW="$(date +'%Y%m%d-%H%M%S')"
+        BACKUP_NAME="projects_backup_${NOW}.tar.gz"
+
+        echo "Backing up '$PROJECTS_DIRECTORY' to '$BACKUP_NAME' now..."
+
+        tar cvzf \
+            "$BACKUPS_DIRECTORY/$BACKUP_NAME" --exclude "node_modules" --exclude "gems" --exclude "Pods" --exclude "vendor" --exclude "tmp" --exclude "build" --exclude ".DS_Store" \
+            "$PROJECTS_DIRECTORY"
+    fi
+}
