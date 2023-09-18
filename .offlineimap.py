e17349ffdfde32f3fd64c5eabe34c1c4847e3b0b
TJ Maynes <tj@tjmaynes.com>
2023-09-17 22:18:36 -0500

    chore(offlineimap): cleanup

--- .offlineimap.py
+++ .offlineimap.py
@@ -4,5 +4,5 @@ import re, os
 def mailpasswd(machine, login, port):
     lookup_string = "machine %s login %s port %s password ([^ ]*)" % (machine, login, port)
     phrase = re.compile(lookup_string)
-    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
+    authinfo = os.popen("gpg -q --no-tty -d $HOME/.authinfo.gpg").read()
     return phrase.search(authinfo).group(1)
