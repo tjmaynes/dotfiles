bbd7436c84a00e0ce3dc17121c65c7ecfeab3b39
TJ Maynes <tj@tjmaynes.com>
2023-10-08 11:30:39 -0500

    refactor(custom-fns): cleanup workspace backup function

--- .custom-fns.sh
+++ .custom-fns.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-function backup_projects()
+function backup_workspace()
 {
-  if [[ -z "$PROJECTS_DIRECTORY" ]]; then
-    echo "Please set 'PROJECTS_DIRECTORY' environment variable before running this command..."
+  if [[ -z "$WORKSPACE_DIRECTORY" ]]; then
+    echo "Please set 'WORKSPACE_DIRECTORY' environment variable before running this command..."
   elif [[ -z "$BACKUPS_DIRECTORY" ]]; then
     echo "Please set 'BACKUPS_DIRECTORY' environment variable before running this command..."
   else
     NOW="$(date +'%Y%m%d-%H%M%S')"
-    BACKUP_NAME="projects_backup_${NOW}.tar.gz"
+    BACKUP_NAME="workspace_backup_${NOW}.tar.gz"
 
-    echo "Backing up '$PROJECTS_DIRECTORY' to '$BACKUP_NAME' now..."
+    echo "Backing up '$WORKSPACE_DIRECTORY' to '$BACKUP_NAME' now... $BACKUPS_DIRECTORY/$BACKUP_NAME"
 
     tar cvzf \
-      "$BACKUPS_DIRECTORY/$BACKUP_NAME" --exclude "node_modules" --exclude "gems" --exclude "Pods" --exclude "vendor" --exclude "tmp" --exclude "build" --exclude ".DS_Store" \
-       -C "$PROJECTS_DIRECTORY"
+      "$BACKUPS_DIRECTORY/$BACKUP_NAME" --exclude "node_modules" --exclude ".cache" --exclude "gems" --exclude "Pods" --exclude "vendor" --exclude "tmp" --exclude "build" --exclude ".DS_Store" \
+       -C "$WORKSPACE_DIRECTORY" .
   fi
 }
 
 function backup_all() {
-  backup_projects
+  backup_workspace
 }
