4c1165927f8b3f3b7b453ef84a0e476363d16283
TJ Maynes <tj@tjmaynes.com>
2024-07-28 22:55:17 -0500

    chore: remove backups for now

--- .custom-fns.sh
+++ .custom-fns.sh
@@ -7,28 +7,6 @@ function kill-process-on-port() {
   fi
 }
 
-function backup-workspace()
-{
-  if [[ -z "$WORKSPACE_DIRECTORY" ]]; then
-    echo "Please set 'WORKSPACE_DIRECTORY' environment variable before running this command..."
-  elif [[ -z "$BACKUPS_DIRECTORY" ]]; then
-    echo "Please set 'BACKUPS_DIRECTORY' environment variable before running this command..."
-  else
-    NOW="$(date +'%Y%m%d-%H%M%S')"
-    BACKUP_NAME="workspace_backup_${NOW}.tar.gz"
-
-    echo "Backing up '$WORKSPACE_DIRECTORY' to '$BACKUP_NAME' now... $BACKUPS_DIRECTORY/$BACKUP_NAME"
-
-    tar cvzf \
-      "$BACKUPS_DIRECTORY/$BACKUP_NAME" --exclude "node_modules" --exclude ".cache" --exclude "gems" --exclude "Pods" --exclude "vendor" --exclude "tmp" --exclude "build" --exclude ".DS_Store" \
-      -C "$WORKSPACE_DIRECTORY" .
-  fi
-}
-
-function backup-all() {
-  backup-workspace
-}
-
 function convert-m4a-to-mp3() {
   DIRECTORY=$(basename "$PWD")
   mkdir -p "$DIRECTORY"
